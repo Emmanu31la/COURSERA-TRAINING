ac586c4c541e64cf4c782f74b5d14c47c7dbc473
Emmanuella <emmanuellagodwithus26@gmail.com>
2025-12-21 20:31:33 +0100

    Final Modifications
    Removed background image and changed it to gradient. Fixed ticking sound.

---
+++ script.js
@@ -0,0 +1,23 @@
+function updateClock() {
+    const now = new Date();
+
+    let hours = now.getHours();
+    let minutes = now.getMinutes();
+    let seconds = now.getSeconds();
+
+    hours = hours < 10 ? "0" + hours : hours;
+    minutes = minutes < 10? "0" + minutes : minutes;
+    seconds = seconds < 10? "0" + seconds : seconds;
+
+    const timeString = `${hours}:${minutes}:${seconds}`;
+
+    document.getElementById("clock-display").innerText = timeString;
+    const tick = document.getElementById("tick-sound");
+    tick.currentTime = 0;
+    tick.play().catch(error => {
+        console.log("Waiting for thr user to click the page to enable sound...");
+    });
+}
+
+setInterval(updateClock, 1000);
+updateClock();
